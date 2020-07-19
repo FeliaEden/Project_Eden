53180b64d9db6c3b17667481dac1a4897a6559dc
Kyle Bennett <Arlin123321@hotmail.com>
2020-07-19 14:29:18 -0700

    Start of the project
    Real start of project

---
+++ Project Trailsfinder/Assets/Scripts/TabSelection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabSelection : MonoBehaviour
+{
+
+    [SerializeField] private string selectableTag = "Enemy";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material enemyMaterial;
+
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_selection != null)
+        {
+            var selectionRender = _selection.GetComponent<Renderer>();
+            selectionRender.material = enemyMaterial;
+            _selection = null;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform;
+                if (selection.CompareTag(selectableTag))
+                {
+                    var selectionRender = selection.GetComponent<Renderer>();
+                    if (selectionRender != null)
+                    {
+                        selectionRender.material = highlightMaterial;
+                    }
+                    _selection = selection;
+                }
+
+            }
+        }
+    }
+}
