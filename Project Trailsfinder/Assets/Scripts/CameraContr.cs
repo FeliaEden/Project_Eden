6b40b2d19b58c6785153da13f8027f40cdfe36c1
Kyle Bennett <Arlin123321@hotmail.com>
2020-07-19 14:41:10 -0700

    Start of project
    FOR REAL

---
+++ Project Trailsfinder/Assets/Scripts/CameraContr.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContr : MonoBehaviour
+{
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform lookAt;
+    public Transform camTrans;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 5.0f;
+    private float sensitivityY = 4.0f;
+    private int zoomSensitivity = 2;
+
+    private void Start()
+    {
+        camTrans = transform;
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            currentX += Input.GetAxis("Mouse X") * sensitivityX;
+            currentY += Input.GetAxis("Mouse Y") * sensitivityY;
+
+            currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            GetComponent<Camera>().fieldOfView++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Camera>().fieldOfView--;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTrans.position = lookAt.position + rotation * dir;
+        camTrans.LookAt(lookAt.position);
+    }
+}
---
+++ Project Trailsfinder/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public Rigidbody rb;
+    private float forwardForce = 10f;
+    private Camera cam;
+    private CharacterController charContr;
+    private Transform camTransform;
+    private float inputX, inputZ, speeD;
+
+    private Vector3 desiredMoveDir;
+
+    [SerializeField] float rotationSpeed = 0.3f;
+    [SerializeField] float allowRot = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charContr = GetComponent<CharacterController>();
+        cam = Camera.main;
+    }
+
+
+    // On Update per frame, cube moves
+    void FixedUpdate()
+    {
+
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        InputDecider();
+        
+        // Key Inputs
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0f, 0f, forwardForce * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            transform.Translate(0f, 0f, forwardForce * Time.deltaTime);
+        }
+        else if (Input.GetKey("a"))
+        {
+
+            transform.Translate(0f, 0f, forwardForce * Time.deltaTime);
+        }
+        else if (Input.GetKey("s"))
+        {
+            transform.Translate(0f, 0f, forwardForce * Time.deltaTime);
+        }
+
+
+        
+    }
+
+    void InputDecider()
+    {
+        speeD = new Vector2(inputX, inputZ).sqrMagnitude;
+        if(speeD > allowRot)
+        {
+            MovementManager();
+        }
+
+    }
+
+    void MovementManager()
+    {
+
+
+        var camForward = cam.transform.forward;
+        var camRight = cam.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward.Normalize();
+        camRight.Normalize();
+
+        desiredMoveDir = camForward * inputZ + camRight * inputX;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDir), rotationSpeed);
+    }
+
+}
